f6685560b1eaf62afb1ff5a60a48423447f019e3
Abrarulctg <jubairctg99@gmail.com>
2024-05-12 00:45:07 +0600

    Added Recent section

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -11,7 +11,22 @@ const Home = () => {
             <Banner></Banner>
             <HowItWorks></HowItWorks>
 
+            {/* Recent Jobs*/}
+            <div className='flex flex-col lg:flex-row gap-4 items-center my-12'>
+                <div className='w-full lg:w-2/5'>
+                    <img src={jobBanner} alt="" />
+                </div>
+                <div className='w-full lg:w-3/5 p-4 space-y-6'>
+                    <p className='font-ubuntu text-[#2847FF]'>Recent Jobs</p>
+                    <h1 className='text-3xl lg:text-5xl font-ubuntu font-extrabold '>Over all 1000+ Talented People Registered in Our Website</h1>
+                    <p className='text-ubuntu'>
+                        Join over 1000 talented individuals who have registered on our website. Don't miss out – sign up today and unlock endless opportunities!
+                    </p>
+                    <Link to='register'><button className='btn btn-primary bg-[#2847ff] my-8'>Join Now</button></Link>
+                </div>
 
+
+            </div>
         </div>
     );
 };
